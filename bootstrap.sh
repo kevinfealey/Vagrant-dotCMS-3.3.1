3aa80260f97f0ac137da067b201f98c31274a404
kevinfealey <kevin.fealey@aspectsecurity.com>
2017-01-27 09:21:06 -0500

    removed sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,8 +43,8 @@ fi
 
 if [ "$installMySQL" = "true" ]; then
 	echo "Installing MySQL"
-	sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-	sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+	debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+	debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 	apt-get -y install mysql-server
 	apt-get -y install mysql-client
 
